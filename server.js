bc9c3b427d3519f5fcf369628e3b14b8c6d7b00d
kevnviek303@yahoo.com <kevnivek303@yahoo.com>
2022-03-31 16:11:31 -0400

    synced models

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 // import sequelize connection
+const connection = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,8 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+// app.get("*", (req, res) => {
+//   res.json({ ok: "hello world" });
+// });
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+connection.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
